4f5c7dc10b8e01b2c590a1a9cf82c223465b3a94
raphhawk <bing619.at@gmail.com>
2023-06-26 12:54:52 +0530

    jwt init

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,12 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+
 import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-errors";
 import { BadRequestError } from "../errors/bad-request-error";
+
+//kubectl create secret generic jwt-secret --from-literal=JWT_KEY=asdf
 
 const router = express.Router();
 
@@ -29,6 +33,21 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store jwt on session object
+    req.session = {
+      jwt: userJwt,
+    };
+
     res.status(201).send(user);
   }
 );
