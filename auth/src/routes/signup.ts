ef6fc9efc172e9dc19b31ae9c8f6069a078fb5da
raphhawk <bing619.at@gmail.com>
2023-06-28 19:29:23 +0530

    signin init

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,11 +1,10 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
+import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 
 import { User } from "../models/user";
-import { RequestValidationError } from "../errors/request-validation-errors";
 import { BadRequestError } from "../errors/bad-request-error";
-
+import { validateRequest } from "../middlewares/validate-request";
 //kubectl create secret generic jwt-secret --from-literal=JWT_KEY=asdf
 
 const router = express.Router();
@@ -19,12 +18,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
