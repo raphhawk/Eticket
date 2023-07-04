2b36f3128c8f8d69bf4cbe19e9c1aade1a0fba89
raphhawk <bing619.at@gmail.com>
2023-07-04 22:27:53 +0530

    next app init

---
+++ client/pages/index.js
@@ -0,0 +1,9 @@
+const Landing = () => {
+  return (
+    <h1 className="text-2xl bg-teal-500 w-fit px-4 m-4 rounded-md text-white">
+      Landing Page
+    </h1>
+  );
+};
+
+export default Landing;
