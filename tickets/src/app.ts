8f882b2a155263c88721bb821de480c8203ff016
raphhawk <bing619.at@gmail.com>
2023-07-21 10:20:19 +0530

    tickets show complete

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@rh-eticket/common";
 
 import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
 
 const app = express();
 app.set("trust proxy", true);
@@ -17,6 +18,7 @@ app.use(
 );
 app.use(currentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -16,6 +16,7 @@ const start = async () => {
   } catch (err) {
     console.error(err);
   }
+
   app.listen(3000, () => {
     console.log("Listening on port: 3000 [Tickets]");
   });
