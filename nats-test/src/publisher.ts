d64879991ee1a94253e619061fbc4b0bfa9459b6
raphhawk <bing619.at@gmail.com>
2023-07-23 19:34:26 +0530

    added publisher nats

---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,22 @@
+import { log } from "console";
+import nats from "node-nats-streaming";
+
+console.clear();
+
+const stan = nats.connect("eticket", "abc", {
+  url: "http://localhost:4222",
+});
+
+stan.on("connect", () => {
+  console.log("Publisher connected to NATS");
+
+  const data = JSON.stringify({
+    id: "123",
+    title: "concert",
+    price: 20,
+  });
+
+  stan.publish("ticket:created", data, () => {
+    console.log("Event published");
+  });
+});
