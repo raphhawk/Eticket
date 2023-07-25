ee995bba852069e81302e04c692109b8d59aaf0f
raphhawk <bing619.at@gmail.com>
2023-07-25 18:32:54 +0530

    nats shared lib

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from "node-nats-streaming";
+import nats, { Message, Stan, SubscriptionOptions } from "node-nats-streaming";
 import { randomBytes } from "crypto";
 
 console.clear();
@@ -7,10 +7,19 @@ const stan = nats.connect("eticket", randomBytes(4).toString("hex"), {
   url: "http:localhost:4222",
 });
 
-const options = stan.subscriptionOptions().setManualAckMode(true);
+const options = stan
+  .subscriptionOptions()
+  .setManualAckMode(true)
+  .setDeliverAllAvailable()
+  .setDurableName("acc-srv");
 
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
+
+  stan.on("close", () => {
+    console.log("NATS connection closed!");
+    process.exit();
+  });
 
   const subscription = stan.subscribe(
     "ticket:created",
@@ -26,3 +35,49 @@ stan.on("connect", () => {
     msg.ack();
   });
 });
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
+
+abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract onMessage(parsedData: any, msg: Message): void;
+
+  private client: Stan;
+  protected ackWait = 5 * 1000;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === "string"
+      ? JSON.parse(data)
+      : JSON.parse(data.toString("utf8"));
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on("message", (msg: Message) => {
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg);
+    });
+  }
+}
